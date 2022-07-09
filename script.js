f342ddd21b7cd205873aa5dfa58436dc7f63e56f
joaosam <joao99.jp@gmail.com>
2022-07-09 14:56:12 -0300

    Update verify input

--- script.js
+++ script.js
@@ -10,25 +10,51 @@ ScrollReveal({
   duration: 1700
 }).reveal(`#imgDescriptionLeft1, #imgDescriptionLeft2`)
 
+function verifyValueInput() {
+  let valueEmail = document.getElementById('email')
+  let valueInput = valueEmail.value
+  let button = document.getElementById('buttonInput')
+
+  if (valueInput) {
+    console.log('entrou')
+    button.disabled = false
+    button.classList.add('hover')
+  }
+}
+
 function sendInput() {
   let valueEmail = document.getElementById('email')
   let valueInput = valueEmail.value
+  let button = document.getElementById('buttonInput')
 
+  let regex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm
+  let resultRegex = valueInput.match(regex)
+
+  console.log('valueInput', valueInput)
   if (valueInput) {
-    swal({
-      title: 'Boa!',
-      text: 'Obrigado por se inscrever.',
-      icon: 'success',
-      button: 'Fechar'
-    })
-  } else {
-    swal({
-      title: 'Erro!',
-      text: 'Favor inserir um e-mail.',
-      icon: 'error',
-      button: 'Fechar'
-    })
+    button.disabled = false
+    if (resultRegex != null) {
+      swal({
+        title: 'Boa!',
+        text: 'Obrigado por se inscrever.',
+        icon: 'success',
+        button: 'Fechar'
+      })
+    } else {
+      swal({
+        title: 'Erro!',
+        text: 'Favor inserir um e-mail válido.',
+        icon: 'error',
+        button: 'Fechar'
+      })
+    }
   }
-  setTimeout(() => (valueEmail.value = ''), 800)
-  console.log(valueInput)
+
+  setTimeout(
+    () => (valueEmail.value = ''),
+    (valueInput = ''),
+    (button.disabled = true),
+    button.classList.remove('hover'),
+    800
+  )
 }
